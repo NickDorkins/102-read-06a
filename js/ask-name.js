9462bb3d09af3d7cac27b4718571eaec1e5d9625
NickDorkins <nick.dorkins@gmail.com>
2020-04-08 18:04:17 -0700

    102-read-06a

---
+++ js/ask-name.js
@@ -0,0 +1,5 @@
+function askUsername(){
+var username = prompt('What is your name?')
+
+document.write('<h3>' + 'Hello' + 'username' '</h3>');
+}
---
+++ js/ask-preference.js
@@ -0,0 +1,13 @@
+var response = prompt('Do you prefer dogs, cats, or both?')
+
+var message;
+
+if (response === 'dogs') {
+    message = 'I love dogs!'
+} else if ( response === 'cats') {
+    message ==='Cats are great!'
+} else if (response === both) {
+    message === 'Dogs are the best!'
+} else (response === 'Do you like animals?')
+
+document.write('<h3>' + 'Hello' + 'username' '</h3>')
